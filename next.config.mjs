07c307cdff7119a752641d8b5ff819ba3670977f
Peter Paleev <petpal005@gmail.com>
2024-11-21 02:38:48 +0300

    next js deploy

--- next.config.mjs
+++ next.config.mjs
@@ -6,5 +6,14 @@ import webpack from "./webpack.config.mjs";
 const nextConfig = JSON.parse(fs.readFileSync("./next.config.json", "utf-8"));
 nextConfig.webpack = webpack;
 
+// Add basePath and assetPrefix for GitHub Pages
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  nextConfig.basePath = `/${repo}`
+  nextConfig.assetPrefix = `/${repo}/`
+}
+
 // use withLlamaIndex to add necessary modifications for llamaindex library
 export default withLlamaIndex(nextConfig);
