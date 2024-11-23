a40ce3719d8a6c421255bf1286660e26c4453c40
Peter Paleev <petpal005@gmail.com>
2024-11-23 23:11:11 +0300

    logo style update

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,7 +7,7 @@ const basePath = process.env.NEXT_PUBLIC_GITHUB_PAGES === 'true'
 export default function Header() {
   return (
     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <div className="fixed bottom-0 left-0 mb-4 flex h-auto w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:w-auto lg:bg-none lg:mb-0">
+      <div className="bottom-0 left-0 mb-4 flex h-auto w-full items-end justify-center bg-gradient-to-t via-white  lg:static lg:w-auto lg:bg-none lg:mb-0">
         <a
           href="https://www.zemelah.online/"
           className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
