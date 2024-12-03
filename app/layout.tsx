43861f73ec5c4ba431e4fe6df82cc238957ab782
Peter Paleev <petpal005@gmail.com>
2024-12-03 15:30:27 +0300

    overcome synchronys script loading error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./markdown.css";
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,13 +29,13 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
-        <script src="https://cdn.amplitude.com/script/b5dfe188b09294626d09495d89f35653.js"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
+        <Script src="https://cdn.amplitude.com/script/b5dfe188b09294626d09495d89f35653.js" strategy="afterInteractive" />
+        <Script id="amplitude-init" strategy="afterInteractive">
+          {`
             window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1}));
             window.amplitude.init('b5dfe188b09294626d09495d89f35653', {"fetchRemoteConfig":true,"autocapture":true});
-          `
-        }} />
+          `}
+        </Script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
