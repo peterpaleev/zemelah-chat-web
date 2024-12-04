82cd70dc796ef204402fdb2a1d9bb7caee418fbd
Peter Paleev <petpal005@gmail.com>
2024-12-05 01:10:54 +0300

    beta symbol on mobile

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,13 +7,13 @@ const basePath = process.env.NEXT_PUBLIC_GITHUB_PAGES === 'true'
 export default function Header() {
   return (
     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <div className="bottom-0 left-0 mb-4 flex h-auto w-full items-end justify-center bg-gradient-to-t via-white  lg:static lg:w-auto lg:bg-none lg:mb-0">
+      <div className="bottom-0 left-0 mb-4 flex h-auto w-full items-end justify-center bg-gradient-to-t via-white lg:static lg:w-auto lg:bg-none lg:mb-0">
         <a
           href="https://www.zemelah.online/"
-          className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
+          className="flex items-center justify-center font-nunito text-lg font-bold gap-2 px-4"
         >
-          <span>Умный поиск по базе Земелах</span>
-          <div className="relative">
+          <span className="text-center">Умный поиск по базе Земелах</span>
+          <div className="relative flex-shrink-0">
             <Image
               className="rounded-xl"
               src={`${basePath}/zemelah-logo.png`}
@@ -22,7 +22,7 @@ export default function Header() {
               height={40}
               priority
             />
-            <span className="absolute -top-2 -right-10 text-xs font-bold text-gray-500">BETA</span>
+            <span className="absolute -top-2 -right-9 text-xs font-bold text-gray-500">BETA</span>
           </div>
         </a>
       </div>
